13e07ff8588a22d670c60447f9802ef102a618e3
SIDDHARTH PILLALAMARRI <siddhu@SIDDHARTHs-MacBook-Air.local>
2026-07-08 15:41:36 +0530

    Restore working frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
- import SymptomForm from "./components/SymptomForm/SymptomForm";
+import SymptomForm from "./components/SymptomForm/SymptomForm";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <SymptomForm/>
+      <SymptomForm />
     </>
   );
 }
 
-export default App;cd /Users/siddhu/Desktop/Projects/SmartHealthAI
-find . -name "HowItWorks.jsx"
+export default App;
